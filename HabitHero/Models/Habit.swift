86c6ad4b3ccac5ce24069ccfc80a2c1cf9fe30e5
Amandamandaanda <amandatanaskovic@gmail.com>
2026-04-27 13:27:18 +0200

    Added dataclassmodel habit with properties.

--- HabitHero/Models/Habit.swift
+++ HabitHero/Models/Habit.swift
@@ -5,3 +5,21 @@
 //  Created by Amanda Tana on 2026-04-27.
 //
 
+import SwiftData
+import Foundation
+
+@Model
+class Habit {
+    var ID: UUID
+    var streak: Int
+    var name: String
+    var lastPerformed: Date
+    
+    init(ID: UUID, streak: Int, name: String, lastPerformed: Date) {
+        self.ID = ID
+        self.streak = streak
+        self.name = name
+        self.lastPerformed = lastPerformed
+    }
+    
+}
