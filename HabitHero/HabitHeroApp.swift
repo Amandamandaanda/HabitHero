cd87a999dfbb7a26c387833e13a66fb801c0bcc6
Amandamandaanda <amandatanaskovic@gmail.com>
2026-04-27 13:14:18 +0200

    Initial Commit

---
+++ HabitHero/HabitHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHeroApp.swift
+//  HabitHero
+//
+//  Created by Amanda Tana on 2026-04-27.
+//
+
+import SwiftUI
+
+@main
+struct HabitHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
